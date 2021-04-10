2c0439880c2cc807c5d384e0aaf18274173c75e3
tanigawahideaki <80952139+tanigawahideaki@users.noreply.github.com>
2021-04-10 16:23:21 +0900

    Update main.js
    メモを書いた

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function(){
-    $('.dropdwn li').hover(function(){
+    $('.dropdwn li').hover(function(){ //カーソルが乗った時の処理
+        console.log($("ul:not(animated",this));
         $("ul:not(:animated)", this).slideDown();
     }, function(){
         $("ul.dropdwn_menu",this).slideUp();
